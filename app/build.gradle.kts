b7eb7aaa3e8e3b8ac3135f108f37223100280e6d
ARATABARA <florideongerusenge9@gmail.com>
2026-08-18 11:30:25 +0200

    Fix Android Gradle configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,10 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.devtools.ksp")
+}
+
 android {
     namespace = "com.abbless.app"
     compileSdk = 35
@@ -6,6 +13,7 @@ android {
         applicationId = "com.abbless.app"
         minSdk = 24
         targetSdk = 35
+
         versionCode = 1
         versionName = "1.0"
     }
@@ -19,6 +27,51 @@ android {
         jvmToolchain(17)
     }
 
-    // izindi settings...
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.15.0")
+
+    implementation("androidx.activity:activity-compose:1.9.3")
+
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2024.12.01"
+        )
+    )
+
+    implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.compose.ui:ui")
+
+    implementation(
+        "androidx.navigation:navigation-compose:2.8.5"
+    )
+
+    implementation(
+        "androidx.datastore:datastore-preferences:1.1.1"
+    )
+
+    implementation(
+        "androidx.room:room-runtime:2.6.1"
+    )
+
+    implementation(
+        "androidx.room:room-ktx:2.6.1"
+    )
+
+    ksp(
+        "androidx.room:room-compiler:2.6.1"
+    )
 }
 
