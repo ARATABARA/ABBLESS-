b66c9e602354ddcc4494058f8b3980a609cf5bc7
ARATABARA <florideongerusenge9@gmail.com>
2026-08-24 13:38:16 +0200

    Start ABBLESS with registration

--- app/src/main/java/com/abbless/app/core/navigation/AppNavigation.kt
+++ app/src/main/java/com/abbless/app/core/navigation/AppNavigation.kt
@@ -31,7 +31,7 @@ fun AppNavigation() {
 
     NavHost(
         navController = navController,
-        startDestination = Routes.SPLASH
+        startDestination = Routes.REGISTER
     ) {
 
         // =========================
