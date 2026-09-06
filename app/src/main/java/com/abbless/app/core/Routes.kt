56afc191275b7198ce4d2f829477ed09a0323787
ARATABARA <florideongerusenge9@gmail.com>
2026-09-06 15:44:46 +0200

    Fix forgot password route

--- app/src/main/java/com/abbless/app/core/Routes.kt
+++ app/src/main/java/com/abbless/app/core/Routes.kt
@@ -6,6 +6,7 @@ object Routes {
     const val REGISTER = "register"
     const val PRESENTATION = "presentation"
     const val LOGIN = "login"
+    const val FORGOT_PASSWORD = "forgot_password"
     const val HOME = "home"
 
     const val LESSONS = "lessons"
